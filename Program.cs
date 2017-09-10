89e28efb2a41e6a1c61c704b9e7c2fb68bda3f24
MikeyPeters22 <10010931@bcs.net.nz>
2017-09-11 11:42:45 +1200

    test2

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace C2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program will display the division tables of 1 to 12 bases on the number input");
+            Console.WriteLine("Please input a number less than 50");
+            var userChoice = double.Parse(Console.ReadLine());
+            Console.WriteLine(userChoice);
+            
+            Console.WriteLine($"{userChoice} * 1 = {""}"); //Forgot how to display anwser
+            
+
+        }
+    }
+}
